27a333edf58c23f86fbd80baf5feeb7e71c2c5ac
luisfernando2419 <160787936+luisfernando2419@users.noreply.github.com>
2024-10-11 22:23:41 -0600

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,7 @@
+CREATE TABLE CASAS(
+ID_CASA INTEGER IDENTITY PRIMARY KEY,
+nombreCasa NVARCHAR(50),
+ubicacion NVARCHAR(50),
+precio INT,
+metrosCuadradosTerreno INT,
+metrosCuadradosConstruccion INT,)
